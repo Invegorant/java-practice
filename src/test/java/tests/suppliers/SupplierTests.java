d912ab46d226af1f925f22505923b9510501d96d
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-11-02 11:19:57 +0700

    add supplier tests

--- src/test/java/tests/suppliers/SupplierTests.java
+++ src/test/java/tests/suppliers/SupplierTests.java
@@ -1,4 +1,54 @@
 package tests.suppliers;
 
-public class SupplierTests {
+import io.qameta.allure.Description;
+import mainPackage.config.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.*;
+
+public class SupplierTests extends BaseTest {
+    @Test
+    @Description("simple supplier test with math random")
+    void simpleSupplierTest() {
+        Supplier<Double> doubleSupplier = () -> Math.random();
+        System.out.println(doubleSupplier.get());
+    }
+
+    @Test
+    @Description("doubleSupplier test")
+    void doubleSupplierTest() {
+        DoubleSupplier doubleSupplier = Math::random;
+        System.out.println(doubleSupplier.getAsDouble());
+    }
+
+    @Test
+    @Description("IntSupplier test")
+    void intSupplierTest() {
+        IntSupplier intSupplier = () -> ThreadLocalRandom.current().nextInt(0, 6547831);
+        System.out.println(intSupplier.getAsInt());
+    }
+
+    @Test
+    @Description("LongSupplier test")
+    void longSupplierTest() {
+        LongSupplier longSupplier = () ->  ThreadLocalRandom.current().nextLong();
+        System.out.println(longSupplier.getAsLong());
+    }
+
+    @Test
+    @Description("BooleanSupplier test")
+    void booleanSupplierTest() {
+        BooleanSupplier booleanSupplier = () -> ThreadLocalRandom.current().nextBoolean();
+        System.out.println(booleanSupplier.getAsBoolean());
+    }
+
+    @Test
+    @Description("Supplier to uppercase test")
+    void supplierToUpperCaseTest () {
+        String t = "One";
+        Supplier<String> supplierString = () -> t.toUpperCase();
+        System.out.println(supplierString.get());
+    }
+
 }
