07896445959742cb0cd972d45d62d7dbe1792710
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-11-02 10:35:12 +0700

    add consumer tests

---
+++ src/test/java/tests/consumers/ConsumerTests.java
@@ -0,0 +1,36 @@
+package tests.consumers;
+
+import io.qameta.allure.Description;
+import mainPackage.config.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class ConsumerTests extends BaseTest {
+
+    @Test
+    @Description("simple test - just print consumer")
+    void simplePrintConsumerTest () {
+        Consumer<String> printConsumer = t -> System.out.println(t);
+        Stream<String> cities = Stream.of("Sydney", "Moscow", "Kiev", "London", "NY");
+        cities.forEach(printConsumer);
+    }
+
+    @Test
+    @Description("composition of consumers test")
+    void consumerCompositionTest () {
+        List<String> cities = Arrays.asList("Sydney", "Moscow", "Kiev", "London", "NY", "Ganalulu");
+        Consumer <List<String>> upperCaseConsumer = list -> {
+            for (int i = 0; i < list.size(); i++) {
+                list.set(i, list.get(i).toUpperCase());
+            }
+        };
+        Consumer<List<String>> printConsumer = list -> list.stream().forEach(System.out::println);
+        upperCaseConsumer
+                .andThen(printConsumer).accept(cities);
+    }
+
+}
---
+++ src/test/java/tests/suppliers/SupplierTests.java
@@ -0,0 +1,4 @@
+package tests.suppliers;
+
+public class SupplierTests {
+}
